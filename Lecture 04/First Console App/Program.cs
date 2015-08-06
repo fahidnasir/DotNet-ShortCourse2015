5a9d29eabb6a647a57ae64bccc4aaa3c722b53c7
Fahid Nasir <fahid.nasir29@hotmail.com>
2015-08-06 18:02:05 +0500

    Lecture 04

---
+++ Lecture 04/First Console App/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Console_App
+{
+    class Program
+    {
+        #region Class Member types
+        const double PI = 3.14159;
+        #endregion
+
+        static void Main(string[] args)
+        {
+            #region Comments
+
+            #endregion
+
+            #region Data Types
+            const double PILOCAL = 3.14159;
+            
+            #region literal types
+            double doubleObj = 3.14159;       /* Simple double object */
+            decimal decimalObj = 85;         /* decimal */
+            //0213       /* octal */
+            //0x4b       /* hexadecimal */
+            int intObj = 30;         /* int */
+            uint uintObj = 30u;        /* unsigned int */
+            long longObj = 30l;        /* long */
+            ulong ulongObj = 30ul;       /* unsigned long */
+            double exponentObj = 314159E-5;    /* exponent of the number */
+            #endregion
+            #endregion
+
+            #region Collections
+
+            #endregion
+
+            #region If Else
+
+            #endregion
+
+            #region For Loop
+
+            #endregion
+
+            #region Foreach Loop
+
+            #endregion
+
+            #region While
+
+            #endregion
+
+            #region try-catch-finally block
+
+            #endregion
+
+            #region using block
+
+            #endregion
+
+            #region Creating Objects of other class
+
+            #endregion
+
+            #region Functions
+
+            #endregion
+        }
+    }
+}
