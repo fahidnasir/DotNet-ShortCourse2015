c8c8566eaf5082e9d523c609942eecec91006a2c
Fahid Nasir <fahid.nasir29@hotmail.com>
2015-09-02 19:25:03 +0500

    WPF Model Binding

---
+++ Lecture 16/WPFModelBinding/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFModelBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new Student() { Age = "105", FirstName="fahid", LastName="nasir"};
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            Student obj = (Student)this.DataContext;
+
+        }
+    }
+}
---
+++ Lecture 16/WPFModelBinding/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CS.Library;
+
+namespace WPFModelBinding
+{
+    public class Student : System.ComponentModel.INotifyPropertyChanged
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value == "abc")
+                    firstName = "xyz";
+                else
+                    firstName = value;
+                
+                OnPropertyChanged("FirstName");
+                OnPropertyChanged("FullName");
+            }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged("LastName");
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        private string fullName;
+
+        public string FullName
+        {
+            get { return FirstName + " ~~ " + LastName; }
+        }
+
+        private string age;
+        public string Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+        #region INotifyPropertyChanged Members
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this,
+                    new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
